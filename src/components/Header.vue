e624f3c7ea612bcfb5dc24a629571f7504554078
Hajime Munakata <hajime.munakata@gmail.com>
2024-08-04 06:12:20 +0900

    headerが出なくなった

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -48,11 +48,10 @@ header{ position: fixed; top: 0; left: 0; margin: 0; width: 100%; height: 70px; 
   align-items: center;
   justify-content: space-between;
 }
-.logo h1{ line-height:70px; color: white; float: right;}
-.nav ul{ list-style-type: none; }
+.logo h1{ color: white; float: right;}
+.nav ul{ list-style-type: none; position: relative; }
 .nav ul li{ float: left; padding: 0; }
 .nav ul li a{
-  line-height: 70px;
   font-family: Montserrat;
   font-size: 14px;/*min10px,max23px*/
   font-weight: 600;
