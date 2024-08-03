e83d2b7fa458f26f2a7771a4c5ae47ac6543fa76
Hajime Munakata <hajime.munakata@gmail.com>
2024-08-04 04:19:22 +0900

    header修正

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,14 +8,14 @@
 
     <body><!-- 実際の中身ここからブラウザ上で表示される -->
       <header>
-        <div>
+        <div class="logo">
           <a href="https://umasiyo.github.io/shakivue/">
             <img src="../assets/logo_white.png" alt="ShakilStudio_logo" class="logo">
-            <!-- <h1>SHAKIL STUDIO</h1> -->
+            <h1>SHAKIL STUDIO</h1>
           </a>
         </div>
         <div>
-          <nav>
+          <nav class="nav">
             <ul class="menu">
               <li><a href="https://vuejs.org" target="_blank" rel="noopener">ABOUT</a></li>
               <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">WORKS</a></li>
@@ -42,23 +42,20 @@ export default {
 
 <!-- ここでのCSSはこのcomponentでしか使用できない -->
 <style scoped>
-html{
-  font-size: 100%;
-}
-
-.header{
+html{ font-size: 100%; }
+.logo{ color: white; }
+header{
   position: fixed;
   top: 0;
   left: 0;
   margin: 0;
   z-index: 10;
-  width: 100lvw;
+  width: 100%;
   height: 70px;
   background-color: #082B71;
   display: flex;
   justify-content: space-between;
 }
-
 .menu li{
   margin: 0;
   height: 70px;
@@ -66,7 +63,6 @@ html{
   float: left;
   list-style: none;
 }
-
 .menu a{
   font-family: Montserrat;
   font-weight: bolder;
@@ -74,11 +70,9 @@ html{
   color: white;
   text-decoration: none;
 }
-
 img.logo {
   width: 37px;
   height: 37px;
   padding: 15px;
 }
-
 </style>
